3415a13b0cf148fd8a803f0f3cb43d11cb9c2931
yukihiro-numata <yuki.numata97@gmail.com>
2022-11-02 00:09:52 +0900

    Update login page mock.

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,3 +1,6 @@
+import 'package:cafe_note_mobile/components/atoms/form_container.dart';
+import 'package:cafe_note_mobile/components/atoms/form_label.dart';
+import 'package:cafe_note_mobile/components/atoms/primary_button.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -8,6 +11,38 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('ログイン'),
+      ),
+      body: Form(
+        child: Container(
+          margin: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const FormLabel(text: 'メールアドレス'),
+              FormContainer(
+                child: TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const FormLabel(text: 'パスワード'),
+              FormContainer(
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              PrimaryButton(
+                buttonLabel: 'ログイン',
+                onPressed: () => print(''),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
