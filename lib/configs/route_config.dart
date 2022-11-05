fdd7505a4154baf72379ab906d6d9a5d4dcc9a36
yukihiro-numata <yuki.numata97@gmail.com>
2022-11-05 21:29:24 +0900

    Remove signup/login pages.

--- lib/configs/route_config.dart
+++ lib/configs/route_config.dart
@@ -3,8 +3,6 @@ import 'package:cafe_note_mobile/pages/cafe/cafe.dart';
 import 'package:cafe_note_mobile/pages/cafes/cafes.dart';
 import 'package:cafe_note_mobile/pages/create_cafe/_basic_info.dart';
 import 'package:cafe_note_mobile/pages/create_cafe/create_cafe.dart';
-import 'package:cafe_note_mobile/pages/login/login.dart';
-import 'package:cafe_note_mobile/pages/signup/signup.dart';
 import 'package:flutter/material.dart';
 
 class RouteConfig {
@@ -13,8 +11,6 @@ class RouteConfig {
   static String cafe = '/cafes/detail';
   static String createCafe = 'cafes/create';
   static String createCafeBasicInfo = 'cafes/create/basic_info';
-  static String signup = '/signup';
-  static String login = '/login';
 
   static Map<String, WidgetBuilder> routes = {
     main: (context) => const BottomMenu(),
@@ -22,8 +18,6 @@ class RouteConfig {
     cafe: (context) => const CafePage(),
     createCafe: (context) => const CreateCafePage(),
     createCafeBasicInfo: (context) => const CreateCafeBasicInfo(),
-    signup: (context) => const SignupPage(),
-    login: (context) => const LoginPage(),
   };
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,11 @@ import 'package:cafe_note_mobile/configs/route_config.dart';
 import 'package:cafe_note_mobile/controllers/cafe_controller.dart';
 import 'package:cafe_note_mobile/controllers/cafes_controller.dart';
 import 'package:cafe_note_mobile/controllers/create_cafe_controller.dart';
-import 'package:cafe_note_mobile/controllers/login_controller.dart';
 import 'package:cafe_note_mobile/controllers/my_page_controller.dart';
-import 'package:cafe_note_mobile/controllers/signup_controller.dart';
 import 'package:cafe_note_mobile/states/cafe_state.dart';
 import 'package:cafe_note_mobile/states/cafes_state.dart';
 import 'package:cafe_note_mobile/states/create_cafe_state.dart';
-import 'package:cafe_note_mobile/states/login_state.dart';
 import 'package:cafe_note_mobile/states/my_page_state.dart';
-import 'package:cafe_note_mobile/states/signup_state.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
@@ -31,12 +27,6 @@ void main() async {
         ),
         StateNotifierProvider<CreateCafeController, CreateCafeState>(
           create: (_) => CreateCafeController(),
-        ),
-        StateNotifierProvider<SignupController, SignupState>(
-          create: (_) => SignupController(),
-        ),
-        StateNotifierProvider<LoginController, LoginState>(
-          create: (_) => LoginController(),
         ),
         StateNotifierProvider<MyPageController, MyPageState>(
           create: (_) => MyPageController(),
