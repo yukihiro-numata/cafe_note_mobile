291f6425b1c8e2348660ee56651ac06cbe5dca4c
yukihiro-numata <yuki.numata97@gmail.com>
2023-02-12 13:40:35 +0900

    Add distribution of api url based on platform is android or ios.

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -1,12 +1,20 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
 class HttpService<T> {
-  final _baseUrl = "http://10.0.2.2:3000";
+  static const apiUrlScheme = "http";
+  static const apiPort = "3000";
+  late String baseUrl;
+
+  HttpService() {
+    baseUrl =
+        "$apiUrlScheme://${Platform.isAndroid ? "10.0.2.2" : "localhost"}:$apiPort";
+  }
 
   Future<Map<String, dynamic>> get(String path) async {
-    final url = Uri.parse(_baseUrl + path);
+    final url = Uri.parse(baseUrl + path);
     final response = await http.get(url);
     // TODO: エラーハンドリング
     return jsonDecode(response.body);
@@ -14,7 +22,7 @@ class HttpService<T> {
 
   Future<Map<String, dynamic>> post(
       String path, Map<String, String?> body) async {
-    final url = Uri.parse(_baseUrl + path);
+    final url = Uri.parse(baseUrl + path);
     final response = await http.post(url, body: body);
     if (response.statusCode != 200) {
       throw Exception();
