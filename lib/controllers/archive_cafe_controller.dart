556f974f0f08e75c3a8a33a84aa116954eb0bed6
yukihiro-numata <yuki.numata97@gmail.com>
2023-04-02 14:57:49 +0900

    Update to upload image files when pressing submit button.

--- lib/controllers/archive_cafe_controller.dart
+++ lib/controllers/archive_cafe_controller.dart
@@ -1,9 +1,13 @@
 import 'dart:io';
 
 import 'package:cafe_note_mobile/states/archive_cafe_state.dart';
+import 'package:cafe_note_mobile/states/auth_state.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as p;
 import 'package:state_notifier/state_notifier.dart';
+import 'package:ulid/ulid.dart';
 
 class ArchiveCafeController extends StateNotifier<ArchiveCafeState>
     with LocatorMixin {
@@ -12,6 +16,8 @@ class ArchiveCafeController extends StateNotifier<ArchiveCafeState>
   final int cafeId;
   final TextEditingController visitedDateFormController =
       TextEditingController();
+
+  AuthState get _authState => read();
 
   ArchiveCafeController({required this.cafeId})
       : super(const ArchiveCafeState());
@@ -46,7 +52,15 @@ class ArchiveCafeController extends StateNotifier<ArchiveCafeState>
     state = currentState.copyWith(images: images);
   }
 
-  void handleSubmitButtonPressed() {
-    print(state);
+  void handleSubmitButtonPressed() async {
+    final storageRef = FirebaseStorage.instance
+        .ref("/cafes/$cafeId/users/${_authState.user?.id}");
+
+    await Future.forEach(state.images, (i) async {
+      final extension = p.extension(i.path);
+      final fileName = Ulid().toString();
+      final imageRef = storageRef.child(p.setExtension(fileName, extension));
+      await imageRef.putFile(i);
+    });
   }
 }
