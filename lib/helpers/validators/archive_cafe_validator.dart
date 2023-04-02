f927d1800eeeca0c43843d5d1e94db063307fcce
yukihiro-numata <yuki.numata97@gmail.com>
2023-04-02 17:58:20 +0900

    Update memo to optional field.

--- lib/helpers/validators/archive_cafe_validator.dart
+++ lib/helpers/validators/archive_cafe_validator.dart
@@ -4,9 +4,6 @@ class ArchiveCafeValidator {
   static const memoMaxLength = 1000;
 
   static String? memo(String? value) {
-    if (CommonValidator.checkIsEmpty(value)) {
-      return CommonValidator.validationMessageRequired;
-    }
     if (CommonValidator.checkDigit(value: value!, digit: memoMaxLength)) {
       return CommonValidator.validationMessageMaxLength(memoMaxLength);
     }
